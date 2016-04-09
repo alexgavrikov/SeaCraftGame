550c5d8ac257db39611fa004370d1386326775e8
alexgavrikov <alex-gavrikov@mail.ru>
2016-04-09 21:37:28 +0300

    Update TClient.cpp

--- TClient.cpp
+++ TClient.cpp
@@ -1,42 +1,55 @@
 /*
  * TClient.cpp
  *
- *  Created on: 9 ΰοπ. 2016 γ.
+ *  Created on: 9 Γ Γ―Γ°. 2016 Γ£.
  *      Author: user
  */
 
 #include "TClient.h"
 
+void TClient::SendMessages() {
 
-  void TClient::Send(const char* data, size_t sz) {
+  // Here we may need some wrapping-routine. Maybe we need to wrap a data
+  // into html-headers-wrapper
+  // Maybe it is not necessary if we use Qt.
+  // CODE HERE
 
-    // Here we may need some wrapping-routine. Maybe we need to wrap a data
-    // into html-headers-wrapper
-    // Maybe it is not necessary if we use Qt.
-    // CODE HERE
-
-    std::unique_lock<std::mutex> send_mutex_wrapper(send_mutex_);
-    for (; sz > 0;) {
-      int res = send(client_socket_, data, sz, 0);
-      if (res <= 0)
-        return;
-      data += res;
-      sz -= res;
-    }
+  std::string whole_message;
+  while (!messages_queue.empty()) {
+    whole_message += messages_queue.dequeue();
+    whole_message += ";";
   }
-
-  // Just check if ships are correct.
-  bool TClient::CorrectShips() const {
-
-    // CODE HERE
-
+  if(whole_message.empty()) {
+    whole_message = "OK";
   }
-
-  // The following function does a change in ships-vector (if necessary)
-  // and returns MISS, HALF, KILL or WIN.
-  size_t TClient::GetShooting(const size_t x_coord, const size_t y_coord) {
-
-    // CODE HERE
-
+  const char* data = whole_message.c_str();
+  size_t sz = whole_message.size();
+  for (; sz > 0;) {
+    int res = send(client_socket_, data, sz, 0);
+    if (res <= 0)
+      return;
+    data += res;
+    sz -= res;
   }
+}
+
+void TClient::PrepareMessage(const std::string& message) {
+  messages_queue.enqueue(message);
+}
+
+// Just check if ships are correct.
+bool TClient::CorrectShips() const {
+
+  // CODE HERE
+
+}
+
+// The following function does a change in ships-vector (if necessary)
+// and returns MISS, HALF, KILL or WIN.
+size_t TClient::GetShooting(const size_t x_coord, const size_t y_coord) {
+
+  // CODE HERE
+
+}
+
 
