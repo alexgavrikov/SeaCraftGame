a05460d2155afcf0b3cd18db588ecd5499bf84f4
alexgavrikov <alex-gavrikov@mail.ru>
2016-05-15 21:48:07 +0300

    refreshed TODO.txt, added port selection through argv

--- server_main.cpp
+++ server_main.cpp
@@ -5,17 +5,24 @@
  *      Author: user
  */
 
+#include <cstdlib>
 #include "server.h"
 
-int main() {
+int main(int argc, char** argv) {
 
 #ifdef _WIN32
   WSADATA wsaData;
   WSAStartup(0x0202, &wsaData);
 #endif
 
+  if (argc != 2) {
+    std::printf("Usage: %s port_to_bind\n", argv[0]);
+    return 1;
+  }
+
+  int port_to_bind = std::atoi(argv[1]);
   Server s;
-  s.Bind(1234, "");
+  s.Bind(port_to_bind, "");
   s.AcceptLoop();
 
   return 0;
